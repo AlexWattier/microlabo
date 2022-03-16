2b8db51123b47a29ad8292f2816cceeb55465dd2
alex1997xd <alex.wattier1997@gmail.com>
2022-03-16 17:13:16 +0100

    modif valeur registre

--- TD 2 - Instructions logiques-20220311/code/1_comprehension_log.asm
+++ TD 2 - Instructions logiques-20220311/code/1_comprehension_log.asm
@@ -1,19 +1,17 @@
-; 1_comprehension_log.asm
-
 global main
 
 section .text
 main:
     mov al, 10011101b                
-    not al              ; al = 01100010b, zf = 0, sf = 1
+    not al              ; al = 01100010b, zf = 0, sf = 0
 
     mov al, 11100101b
     mov ah, 00101010b
-    and al, ah          ; al = 11101111b, zf = 0, sf = 1
+    and al, ah          ; al = 00100000b, zf = 0, sf = 0
 
     mov al, 11100101b
     mov ah, 00001010b
-    and al, ah          ; al = 11101111b, zf = 0, sf = 1
+    and al, ah          ; al = 00000000b, zf = 1, sf = 0
 
     mov al, 01100101b
     mov ah, 01010101b
@@ -26,14 +24,13 @@ main:
     mov dx, 1100010011100101b
     mov si, 0011000001100010b
     xor dx, si          ; dx = 1111010010000111b
-                        ; zf = 0, sf = 1	
+                        ; zf = 0, sf = 1  
 
     mov al, 11100101b
     mov ah, 11100101b
-    xor al, ah          ; al = 00000000b, zf = 0, sf = 0
+    xor al, ah          ; al = 00000000b, zf = 1, sf = 0
 
     ; fin
     mov rax, 60
     mov rdi, 0
     syscall
-
